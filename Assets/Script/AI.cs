ed53fc0ce8bd0e1cd0ccbd498af54508a3c120fe
konlawat-rakt-droid <konlawat.rakt@bumail.net>
2026-03-26 22:33:58 +0700

    Update Map And enemy AI

--- Assets/Script/AI.cs
+++ Assets/Script/AI.cs
@@ -1,16 +1,42 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class AI : MonoBehaviour
+public class Patrol : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform pointA;
+    public Transform pointB;
+
+    public float speed = 3f;
+    public float waitTime = 1f;
+
+    private Transform target;
+
     void Start()
     {
-        
+        target = pointA;
+        StartCoroutine(MoveLoop());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MoveLoop()
     {
-        
+        while (true)
+        {
+            // เดินไป target
+            while (Vector3.Distance(transform.position, target.position) > 0.1f)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    target.position,
+                    speed * Time.deltaTime
+                );
+                yield return null;
+            }
+
+            // หยุด
+            yield return new WaitForSeconds(waitTime);
+
+            // สลับเป้าหมาย
+            target = (target == pointA) ? pointB : pointA;
+        }
     }
 }
